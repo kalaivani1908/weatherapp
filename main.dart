0775d376608140411c88193db5c6e24a1d779de9
kalaivani1908 <sivasundari2004@gmail.com>
2025-07-02 15:25:57 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/loading_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
